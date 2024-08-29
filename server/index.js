7ebf3acef5cd8ea71e7df2cc4f8e2ea5a015267c
santosh <santoshchouhan2442@gmail.com>
2024-08-29 19:09:24 +0530

    seting up server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import { Server } from "socket.io";
+
+const PORT = 9000;
+
+const io = new Server(PORT, {
+    cors:{
+        origin: 'http://localhost:3000/',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', socket => {});
